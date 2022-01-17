fc32167bc3ec1c74612c6b0bff5834832fa1f9e2
Brady Camp <bradyjcamp@gmail.com>
2022-01-17 16:18:23 -0500

    added objects to js and structured html

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let locationData = document.getElementById('dataByLocation');
+console.log(locationData);
+
+let seattle = {
+  location: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  aveCookieSale: 6.3,
+};
+
+let tokyo = {
+  location: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  aveCookieSale: 1.2,
+};
+
+let dubai = {
+  location: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  aveCookieSale: 3.7,
+};
+
+let paris = {
+  location: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  aveCookieSale: 2.3
+};
+
+let lima = {
+  location: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  aveCookieSale: 4.6
+};
