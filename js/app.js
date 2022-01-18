43babe47261321df7dd3cdb6f9af58a428173ddd
Brady Camp <bradyjcamp@gmail.com>
2022-01-18 12:12:35 -0500

    removed extra lines

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let storeSection = document.getElementById('dataByLocation');
-console.log(storeSection);
+// console.log(storeSection);
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
